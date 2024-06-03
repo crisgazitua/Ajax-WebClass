2a4fb075259e990fd26815bea0d1d052ec86b3df
crisgazitua <cggazitua@miuandes.cl>
2024-06-03 10:18:33 -0400

    third commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,10 @@ class UsersController < ApplicationController
 
     def index
         @users = User.all
+        respond_to do |format|
+            format.html
+            format.json { render json: @users }
+            format.js 
+        end
     end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("turbolinks:load", function() {
+    $('#reload_users').on('click', function() {
+      $.ajax({
+        url: '/users',
+        type: 'get',
+        dataType: 'script'
+      });
+    });
+});
