b1ef3d65c09a34a0eb80153fcfa69a37a7d0fb92
crisgazitua <cggazitua@miuandes.cl>
2024-06-03 09:44:20 -0400

    second commit

---
+++ db/migrate/20240603130732_create_table_users.rb
@@ -0,0 +1,10 @@
+class CreateTableUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false, default: ''
+      t.string :email, null: false, default: ''
+
+      t.timestamps
+    end
+  end
+end
